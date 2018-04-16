dd572fb5caf31130cc839d72f99651b6eda63e84
linxiaolong <1394139539@qq.com>
2018-04-16 14:10:35 +0800

    代码整理: 统一上下行数据格式, 移除无用代码

--- Applications/YourApp/Events.php
+++ Applications/YourApp/Events.php
@@ -30,50 +30,95 @@ use \GatewayWorker\Lib\Gateway;
  */
 class Events
 {
+    const EVENT_JOIN = 'join_group';
+
+    /**
+     * 发送数据到客户端格式封装
+     *
+     * @param string $event
+     * @param string $data
+     * @param string $from
+     * @param string $to
+     * @return string
+     */
+    public static function getSendData($event = '', $data = '', $from = '', $to = '') {
+        $sendData = [
+            'event' => $event,
+            'data'  => $data,
+            'from'  => $from,
+            'to'    => $to,
+        ];
+
+        return json_encode($sendData);
+    }
+
     public static function onWorkerStart($businessWorker)
     {
         echo "onWorkerStart: \n\r";
     }
 
     /**
-     * 当客户端连接时触发
+     * 每当客户端连接时触发
      * 如果业务不需此回调可以删除onConnect
      * 
-     * @param int $client_id 连接id
+     * @param int $clientId 连接id
+     * @throws Exception
      */
-    public static function onConnect($client_id) {
+    public static function onConnect($clientId) {
         echo "onConnect: \n\r";
 
-        // 向当前client_id发送数据 
-        Gateway::sendToClient($client_id, "Hello $client_id\n");
+        // 向当前client_id发送数据
+        // Gateway::sendToClient($clientId, self::getSendData('on_connect', "Hello $clientId\n"));
 
         // 向所有人发送
-        Gateway::sendToAll("$client_id login\n");
+        // Gateway::sendToAll(self::getSendData('on_connect', "$clientId login"));
     }
     
    /**
     * 当客户端发来消息时触发
-    * @param int $client_id 连接id
+    * @param int $clientId 连接id
     * @param mixed $message 具体消息
     */
-   public static function onMessage($client_id, $message) {
+   public static function onMessage($clientId, $message) {
        echo "onMessage: \n\r";
 
-       var_dump($message);
+       // TODO: 接收数据时需要针对 $message 不是 json 字符串的情况做处理, 不能报错
+       $clientData = json_decode($message, true);
+
+       switch ($clientData['event']) {
+           // 客户端自定义分组
+           case self::EVENT_JOIN:
+               self::clientJoinGroups($clientId, $clientData['data']);
+               Gateway::sendToClient($clientId, self::getSendData('join_group_response', '加入分组成功'));
+               break;
+       }
 
         // 向所有人发送
-        Gateway::sendToAll("$client_id said = $message");
+//        Gateway::sendToAll("$clientId said = $message");
    }
-   
+
+    /**
+     * 将 client_id 加入某个组，以便通过 Gateway::sendToGroup 发送数据
+     *
+     * @param $clientId
+     * @param $groups
+     */
+   public static function clientJoinGroups($clientId, $groups) {
+       foreach ($groups as $group) {
+           Gateway::joinGroup($clientId, $group);
+       }
+   }
+
    /**
     * 当用户断开连接时触发
-    * @param int $client_id 连接id
+    * @param int $clientId 连接id
+    * @throws Exception
     */
-   public static function onClose($client_id) {
-       echo "onMessage: \n\r";
+   public static function onClose($clientId) {
+       echo "onClose: \n\r";
 
-       // 向所有人发送 
-       GateWay::sendToAll("$client_id logout");
+       // 向所有人发送
+       // Gateway::sendToAll(self::getSendData('on_close', "$clientId logout"));
    }
 
     public static function onWorkerStop($businessWorker)
